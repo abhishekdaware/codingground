fc4d1c1fcd52317eafa184769db53a151f83741a
Abhishek Daware <salesappwithsalesforce@gmail.com>
2018-06-21 16:15:43 +0530

    Created by Coding Ground Client

---
+++ New Project-20180621/Dog.java
@@ -0,0 +1,17 @@
+// Class Declaration
+public class Dog {
+    // Instance Variables
+    String breed;
+    String size;
+    int age;
+    String color;
+
+  
+    // method 1
+    public String getInfo() {
+        return ("Breed is: "+breed+" Size is:"+size+" Age is:"+age+" color is: "+color);
+    }
+    
+
+   
+}
